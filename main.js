9fe762853b7c76680f65c8e725d18d448c098214
Ankit Soni <98032129+Ankit-sony@users.noreply.github.com>
2023-02-23 01:57:32 +0530

    sdds

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let peerConnection;
 const servers = {
     iceServers:[
         {
-            urls:['stun:stun1.l.google.com:19302','stun:stun2.l.google.com:19302' ]
+            urls:['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
         }
     ]
 }

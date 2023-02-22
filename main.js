26079971d5f958a7ff8278dca587f05fc91ae685
Ankit Soni <98032129+Ankit-sony@users.noreply.github.com>
2023-02-22 20:27:48 +0530

    SDP update

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+let App_ID = "4dbfd07eb16e4c599f56063d4126f4b6"
+
+let token = null;
+let uid = String(Math.floor(Math.random() * 10000))
+
+
+let client;
+let channel;
+
+
+
 let localStream;
 let remoteStrem;
 let peerConnection;
@@ -11,15 +22,57 @@ const servers = {
 }
 
 let init = async () => {
+    client = await AgoraRTM.createInstance(App_ID)
+    await client.login({uid, token})
+    
+    //index.html?room=234234
+    channel = client.createChannel('main')
+    await channel.join()
+
+    channel.on('MemberJoined', handleUserJoined)
+
+    client.on('MessageFromPeer', handleMessageFromPeer)
+
     localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false}) 
     document.getElementById('user-1').srcObject =localStream
-    createOffer()
+    
 }
-let createOffer = async () =>{
+
+let handleMessageFromPeer = async (message, MemberId) => {
+    message = JSON.parse(message.text)
+    if(message.type === 'offer'){
+        createAnswer(MemberId, message.offer)
+    }
+
+    if(message.type === 'answer'){
+        addAnswer(message.answer)
+    }
+
+    if(message.type === 'candidate'){
+        if(peerConnection){
+            peerConnection.addIceCandidate(message.candidate)
+        }
+    }
+
+}
+
+
+let handleUserJoined = async (MemberId) =>{
+    console.log('A new user joined the channel:', MemberId)
+    createOffer(MemberId)
+}
+
+let createPeerConnection = async (MemberId) =>{
+
     peerConnection = new RTCPeerConnection(servers)
 
     remoteStrem = new MediaStream()
     document.getElementById('user-2').srcObject = remoteStrem
+
+    if(!localStream){
+        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false}) 
+    document.getElementById('user-1').srcObject =localStream
+    }
 
     localStream.getTracks().forEach((track) =>{
         peerConnection.addTrack(track, localStream)
@@ -27,23 +80,43 @@ let createOffer = async () =>{
 
 
     peerConnection.ontrack = (event) => {
-        event.strems[0].getTracks().forEach((track) => {
-            remoteStrem.addTrack()
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStrem.addTrack(track)
         })
     }
     
     peerConnection.onicecandidate = async (event) => {
-    if(event.candidate){
-        console.log("New ICE candidate:", event.candidate)
+    if(event.candidate)     
+      {client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId)
         }
     }
     
+}
 
-
+let createOffer = async (MemberId) =>{
+    await createPeerConnection(MemberId)
 
     let offer = await peerConnection.createOffer()
     await peerConnection.setLocalDescription(offer)
-    console.log('offer:', offer)
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'offer', 'offer':offer})}, MemberId)
 }
+
+let createAnswer = async(MemberId, offer) =>{
+    await createPeerConnection(MemberId)
+
+    await peerConnection.setRemoteDescription(offer)
+
+    let answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
+
+}
+let addAnswer = async (answer) => {
+    if(!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer)
+    }
+}  
 
 init()
